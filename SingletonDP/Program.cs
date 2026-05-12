5c3fa266ccbdd63a6187ec4dd4ba0c1896ab4eb3
amir <amirmoinirad@gmail.com>
2026-05-12 11:44:18 +0330

    Moved the Singleton class to a new file.

--- SingletonDP/Program.cs
+++ SingletonDP/Program.cs
@@ -1,49 +1,17 @@
-﻿
-// Amir Moeini Rad
+﻿// Amir Moeini Rad
 // September 2025
 
-// Main Concept: The Singleton Design Pattern
+// The Singleton Design Pattern
 // Version: 2.0
 
 // In this pattern, a class has only one instance.
 // This is useful when exactly one object is needed to coordinate actions across the system or application.
-// Examples include database connections pool, logging manager, and configuration settings manager.
 
 // Modern version of the Singleton pattern in C#.
 // The private static readonly field guarantees singularity, thread-safety and lazy initialization by default.
 
-
 namespace SingletonDP
-{
-    public sealed class Singleton
-    {                        
-        // Since the accessor property 'Instance' is static, it cannot access an instance field.
-        // This field implements the concepts of one single object and thread safety.
-        // However, it provides eager initialization, not lazy initialization.
-        private static readonly Singleton _instance = new();        
-
-        
-        // No one can create the Singleton object from outside the class.
-        private Singleton()
-        {
-            Console.WriteLine("Creating the Singleton instance...");
-        }
-
-       
-        // This property provides a global access point to the Singleton instance.
-        public static Singleton Instance
-        {
-            get { return _instance; }            
-        }
-
-        
-        public void ShowMessage() => Console.WriteLine("Hello from the Singleton object!\n");
-    }
-
-
-    ///////////////////////////////////////////
-
-
+{    
     public class Program
     {
         static void Main(string[] args)
@@ -52,7 +20,6 @@ namespace SingletonDP
             Console.WriteLine("The Singleton Design Pattern in C#.NET.");
             Console.WriteLine("---------------------------------------\n");
 
-
             // We cannot create an instance of Singleton directly because its constructor is private.            
             Singleton s1 = Singleton.Instance;
             s1.ShowMessage();
@@ -60,9 +27,7 @@ namespace SingletonDP
             Singleton s2 = Singleton.Instance;
             s2.ShowMessage();
 
-            Console.WriteLine("Are s1 and s2 the same? " + (ReferenceEquals(s1, s2) ? "Yes" : "No"));
-
-
+            Console.WriteLine("\nAre s1 and s2 the same? " + (ReferenceEquals(s1, s2) ? "Yes" : "No"));
             Console.WriteLine("\nDone.");
         }
     }
---
+++ SingletonDP/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace SingletonDP
+{
+    public sealed class Singleton
+    {        
+        // This field implements the concepts of one single object and thread safety.
+        // However, it provides eager initialization, not lazy/late initialization.
+        private static readonly Singleton _instance = new();
+
+        // No one can create the Singleton object from outside the class.
+        private Singleton()
+        {
+            Console.WriteLine("Creating the Singleton instance...");
+        }
+
+        // The global access point to the Singleton instance.
+        public static Singleton Instance
+        {
+            get => _instance;
+        }
+
+        public void ShowMessage() => Console.WriteLine("\nHello from the Singleton object!");
+    }
+}
